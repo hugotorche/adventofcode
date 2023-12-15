9b1c04713be1fce108cf92fa5f6365a2648fb748
htorche <hugo.torche@loccitane.com>
2023-12-15 15:15:08 +0100

    day10 rebase

--- runs/day10.py
+++ runs/day10.py
@@ -21,97 +21,79 @@ JL-.L-7|FJLJFJ'''
 with open('runs/sources/puzzleinput_10.txt', 'r', encoding='utf-8') as puzzleinput:
     puzzle_4 = puzzleinput.read()
 
-symbols_map = {'|': 'NS',
-               '-': 'EW',
-               'L': 'NE',
-               'J': 'NW',
-               '7': 'SW',
-               'F': 'NSE'}
 
-digits_puzzle_re = re.sub('[L|J|7|F|-]', '.', puzzle_3).replace('|', '.')
-digits_matrix = [list(puzzle) for puzzle in digits_puzzle_re.splitlines()]
+def digits_matrix(puzzle):
+    digits_puzzle_re = re.sub('[L|J|7|F|-]', '.', puzzle).replace('|', '.')
+    digits_matrix = [list(puzzle) for puzzle in digits_puzzle_re.splitlines()]
 
-puzzle = puzzle_3.splitlines()
-print('---Init Puzzle:')
-for d in puzzle:
-    print(d)
-lines = len(puzzle)
-columns = len(puzzle[0])
-
-for i in range(lines):
-    for j in range(columns):
-        if puzzle[i][j] == 'S':
-            starting_coor = (i, j)
-            print('---Starting point:')
-            print(starting_coor)
+    return digits_matrix
 
 
-p_coors = [(starting_coor[0], starting_coor[1]-1),
-           (starting_coor[0], starting_coor[1]+1),
-           (starting_coor[0]-1, starting_coor[1]),
-           (starting_coor[0]+1, starting_coor[1])]
-p_coors = [coor for coor in p_coors if coor[1] >= 0]
-next_points = []
-for coor in p_coors:
-    symbol = puzzle[coor[0]][coor[1]]
-    if (coor != starting_coor
-            and symbol != '.'):
-        digits_matrix[coor[0]][coor[1]] = '1'
-        next_points.append(coor)
+def starting_point(puzzle):
+    lines = len(puzzle)
+    columns = len(puzzle[0])
 
-print('---Init Digit Matrix:')
-for d in digits_matrix:
-    print(d)
+    for i in range(lines):
+        for j in range(columns):
+            if puzzle[i][j] == 'S':
+                starting_coor = (i, j)
+                return starting_coor
 
-steps = 0
 
-for i in range(len(next_points)):
+def first_pipe(puzzle, starting_coor, digits_matrix):
+    next_points = [starting_coor]
+
     while len(next_points) > 0:
-        point = next_points[i]
-        point_symbol = puzzle[point[0]][point[1]]
-        point_digit = int(digits_matrix[point[0]][point[1]])
-        p_coors = [(point[0], point[1]-1),
-                   (point[0], point[1]+1),
-                   (point[0]-1, point[1]),
-                   (point[0]+1, point[1])]
-        p_coors = [coor for coor in p_coors if coor[1] >= 0]
-        for coor in p_coors:
-            try:
-                symbol = puzzle[coor[0]][coor[1]]
+        for i in range(len(next_points)):
+            point = next_points[i]
+            point_symbol = puzzle[point[0]][point[1]]
+            point_digit = 0 if point_symbol == 'S' else int(digits_matrix[point[0]][point[1]])
+            p_coors = [(point[0], point[1]-1),
+                       (point[0], point[1]+1),
+                       (point[0]-1, point[1]),
+                       (point[0]+1, point[1])]
+            p_coors = [coor for coor in p_coors if coor[1] >= 0]
+            for coor in p_coors:
+                try:
+                    symbol = puzzle[coor[0]][coor[1]]
+                    if (coor != point and coor != starting_coor
+                            and symbol != '.'
+                            and digits_matrix[coor[0]][coor[1]] == '.'):
+                        if coor == (point[0], point[1]-1):
+                            if symbol in ('-', 'L', 'F') and point_symbol in ('S', '-', 'J', '7'):
+                                digits_matrix[coor[0]][coor[1]] = str(point_digit + 1)
+                                next_points.append(coor)
+                        elif coor == (point[0], point[1]+1):
+                            if symbol in ('-', 'J', '7') and point_symbol in ('S', '-', 'L', 'F'):
+                                digits_matrix[coor[0]][coor[1]] = str(point_digit + 1)
+                                next_points.append(coor)
+                        elif coor == (point[0]-1, point[1]):
+                            if symbol in ('|', '7', 'F') and point_symbol in ('S', '|', 'L', 'J'):
+                                past_digit = 0 if '.' else int(digits_matrix[coor[0]][coor[1]])
+                                digits_matrix[coor[0]][coor[1]] = str(point_digit + 1 + past_digit)
+                                next_points.append(coor)
+                        elif coor == (point[0]+1, point[1]):
+                            if symbol in ('|', 'L', 'J') and point_symbol in ('S', '|', '7', 'F'):
+                                digits_matrix[coor[0]][coor[1]] = str(point_digit + 1)
+                                next_points.append(coor)
+                except IndexError:
+                    pass
 
-                if (coor != point and coor != starting_coor
-                        and symbol != '.'
-                        and digits_matrix[coor[0]][coor[1]] != '2'):
-                    if coor == (point[0], point[1]-1):
-                        if symbol in ('-', 'L', 'F') and point_symbol in ('-', 'J', '7'):
-                            digits_matrix[coor[0]][coor[1]] = str(point_digit + 1)
-                            steps += 1
-                            next_points.append(coor)
-                    elif coor == (point[0], point[1]+1):
-                        if symbol in ('-', 'J', '7') and point_symbol in ('-', 'L', 'F'):
-                            digits_matrix[coor[0]][coor[1]] = str(point_digit + 1)
-                            steps += 1
-                            next_points.append(coor)
-                    elif coor == (point[0]-1, point[1]):
-                        if symbol in ('|', '7', 'F') and point_symbol in ('|', 'L', 'J'):
-                            past_digit = 0 if '.' else int(digits_matrix[coor[0]][coor[1]])
-                            digits_matrix[coor[0]][coor[1]] = str(point_digit + 1 + past_digit)
-                            steps += 1
-                            next_points.append(coor)
-                    elif coor == (point[0]+1, point[1]):
-                        if symbol in ('|', 'L', 'J') and point_symbol in ('|', '7', 'F'):
-                            digits_matrix[coor[0]][coor[1]] = str(point_digit + 1)
-                            steps += 1
-                            next_points.append(coor)
-            except IndexError:
-                pass
-        next_points.pop(i)
+            next_points.pop(i)
+            print(next_points)
+
+    return digits_matrix
 
 
-print('---Result Digit Matrix:')
-for d in digits_matrix:
-    print(d)
+init_digits_matrix = digits_matrix(puzzle_4)
+puzzle = puzzle_4.splitlines()
+starting_point = starting_point(puzzle)
+final_digits_matrix = first_pipe(puzzle, starting_point, init_digits_matrix)
 
-print('---Max Steps:')
-print(max([int(d[i]) for i in range(len(d)) 
-           for d in digits_matrix if d[i] not in ('S', '.')]))
+maxs = []
+for matrice in final_digits_matrix:
+    print(matrice)
+    maxs.append(max([m for m in matrice if m != 'S']))
+
+maxs = [int(m) for m in maxs if m != '.']
+print(max(maxs))
